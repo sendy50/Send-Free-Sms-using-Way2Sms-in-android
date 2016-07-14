cc27647a1785bc1e00cd8b24d3965107b3704e33
HADIYA SANJAY <hsanjay50@gmail.com>
2016-07-14 10:41:15 +0530

    Create send_sms.php

---
+++ send_sms.php
@@ -0,0 +1,11 @@
+
+-- this file put in Htdocs of your xampp.
+
+
+
+<?php
+include('way2sms-api.php');
+
+sendWay2SMS(enter your regitered number,'enter your way2sms password',$_REQUEST['phone'],$_REQUEST['msg']);
+
+?>
